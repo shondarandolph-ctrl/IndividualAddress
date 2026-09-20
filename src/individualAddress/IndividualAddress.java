2cdc7d14cb28a8b34f65144f18c47173aeeca150
shond <shond@LAPTOP-61H1LCUR>
2026-09-20 12:05:13 -0400

    Initial commit-Individual Address Java Program

---
+++ src/individualAddress/IndividualAddress.java
@@ -0,0 +1,22 @@
+package individualAddress;
+
+public class IndividualAddress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+/**
+ * Prints the address information for a fictional person.
+ */
+public class PersonInformation {
+    public static void main(String[] args) {
+        System.out.println("First name: Avery");
+        System.out.println("Last name: Morgan");
+        System.out.println("Street address: 742 Maple Street");
+        System.out.println("City: Cedar Falls");
+        System.out.println("Zip code: 50613");
+    }
+}
---
+++ src/individualAddress/PersonalInformation.java
@@ -0,0 +1,22 @@
+package individualAddress;
+
+public class PersonalInformation {
+
+	public static void main(String[] args) {
+		String firstname = "Marquise";
+		String lastname = "Brown";
+		String streetAdress = "2000 Springfield Drive";
+		String city = "Charlotte";
+		String zipcode = "28212";
+		
+		System.out.println(firstname);
+		System.out.println(lastname);
+		System.out.println(streetAdress);
+		System.out.println(city);
+		System.out.println(zipcode);
+		
+		
+
+	}
+
+}
