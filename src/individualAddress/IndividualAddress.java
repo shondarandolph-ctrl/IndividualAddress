e4d21ebf133020324e1c93f280f04d33eecd5051
shond <shond@LAPTOP-61H1LCUR>
2026-09-20 13:35:29 -0400

    Updated Individual Address program

--- src/individualAddress/IndividualAddress.java
+++ src/individualAddress/IndividualAddress.java
@@ -8,15 +8,3 @@ public class IndividualAddress {
 	}
 
 }
-/**
- * Prints the address information for a fictional person.
- */
-public class PersonInformation {
-    public static void main(String[] args) {
-        System.out.println("First name: Avery");
-        System.out.println("Last name: Morgan");
-        System.out.println("Street address: 742 Maple Street");
-        System.out.println("City: Cedar Falls");
-        System.out.println("Zip code: 50613");
-    }
-}
